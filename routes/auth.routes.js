21ce2b27a04cc630654a571e90fb7547841fe475
WalidBlila <walidbila8@gmail.com>
2021-06-01 17:51:52 +0100

    signup  and login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,61 +1,71 @@
-const { Router } = require('express');
-const router = new Router();
-const bcryptjs = require('bcryptjs');
+const express = require("express");
+const router = express.Router();
+const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
-const User = require('../models/User.model');
-const mongoose = require('mongoose');
+const salt = bcryptjs.genSaltSync(saltRounds);
+const User = require("../models/User.model");
 
-// .get() route ==> to display the signup form to users
-router.get('/signup', (req, res) => res.render('auth/signup'));
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
 
-// .post() route ==> to process form data
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const hashedPassword = bcryptjs.hashSync(password, salt);
+  console.log(`Password hash: ${hashedPassword}`);
+
+  User.create({
+    username,
+    passwordHash: hashedPassword,
+  })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res, next) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
   const { username, password } = req.body;
+  console.log("SESSION =====> ", req.session);
 
-  if (!username || !password) {
-    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
     return;
   }
 
-  // make sure passwords are strong:
-  // const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  // if (!regex.test(password)) {
-  //   res
-  //     .status(500)
-  //     .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
-  //   return;
-  // }
-
-  bcryptjs
-    .genSalt(saltRounds)
-    .then(salt => bcryptjs.hash(password, salt))
-    .then(hashedPassword => {
-      return User.create({
-        // username: username
-        username,
-        // passwordHash => this is the key from the User model
-        //     ^
-        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
-      });
-    })
-    .then(userFromDB => {
-      console.log('Newly created user is: ', userFromDB);
-      res.redirect('/userProfile');
-    })
-    .catch(error => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render('auth/signup', { errorMessage: error.message });
-      } else if (error.code === 11000) {
-        res.status(500).render('auth/signup', {
-          errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Username is not registered. Try with other username.",
         });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // req.session.currentUser = user;
+        res.redirect("/userProfile");
       } else {
-        next(error);
+        res.render("auth/login", { errorMessage: "Incorrect password." });
       }
-    }); // close .catch()
+    })
+    .catch((error) => next(error));
 });
 
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+router.post('/logout', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 module.exports = router;
