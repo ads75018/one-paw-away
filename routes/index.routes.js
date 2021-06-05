fd515d72125e686a7e93fd72604663f7e1b685c0
Adam Ghaoul <adamghaoul@gmail.com>
2021-06-05 11:15:41 +0200

    classifieds

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const Doggo = require("../models/User.model");
+
 /* GET home page */
 // router.get("/", (req, res, next) => {
 //   res.render("index");
@@ -10,12 +12,31 @@ module.exports = router;
 
 router.get("/about", (req, res, next) => res.render("about.hbs"));
 
-router.get("/profile", (req, res, next) => res.render("profile.hbs"));
+router.get("/profile", (req, res, next) => {
+  Doggo.find()
+    .then((allTheDoggosFromDB) => {
+      console.log(allTheDoggosFromDB);
+      res.render("profile.hbs", { doggos: allTheDoggosFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the Doggos from DB:", error);
+      next(error);
+    });
+});
 
 router.get("/classifieds", (req, res, next) => {
-  res.render("classifieds.hbs", {
-    isClassifieds: true,
-  })
+  Doggo.find()
+    .then((allTheDoggosFromDB) => {
+      console.log(allTheDoggosFromDB);
+      res.render("classifieds.hbs", {
+        isClassifieds: true,
+        doggos: allTheDoggosFromDB,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while getting the Doggos from DB:", error);
+      next(error);
+    });
 });
 
 // router.get("/login", (req, res, next) => {
